e31ed42025b9715c006621226b028ffed30e318d
tuxflo <flo.hennig@gmail.com>
2017-01-24 09:47:56 +0100

    Verteilungsübersicht beendet

--- src/distribution.c
+++ src/distribution.c
@@ -7,10 +7,13 @@
 #include "entry.h"
 
 void calculate_hash_values(int machinecount, int elementcount, char *key, uint32_t *node, uint32_t *idx);
+void calculate_hash_values_separate(int machinecount, int elementcount, char *key, uint32_t *node, uint32_t *idx);
+void print_results(int *array, int machinecount, int elementcount);
+int read_and_hash(int *jenkins_hash, int *separate_hash, int machinecount, int elementcount);
 int main(int argc, char **argv) {
     int machinecount, elementcount;
-    char *file = "./test_files/names.txt";
-    int *ret;
+    int *separate_hash;
+    int *jenkins_hash;
     int i, j;
     //memset (ret, 0, sizeof (int) * machinecount);
     if(argc < 3) {
@@ -19,12 +22,13 @@ int main(int argc, char **argv) {
     }
     machinecount = atoi(argv[1]);
     elementcount = atoi(argv[2]);
-    ret = (int *) calloc(machinecount, sizeof(int));
+    jenkins_hash = (int *) calloc(machinecount, sizeof(int));
+    separate_hash = (int *) calloc(machinecount, sizeof(int));
     printf("Machines: %d, Elements: %d\n", machinecount, elementcount);
     FILE *fp;
+    char *file = "./test_files/names.txt";
     char *key, *value, *line = NULL;
     size_t read, len = 0;
-
     fp = fopen(file, "r");
     if (fp == NULL) {
         printf("Error opening file: %s!\n", file);
@@ -50,20 +54,25 @@ int main(int argc, char **argv) {
         int index;
 
         calculate_hash_values(machinecount, elementcount,key, &node, &index);
-        ret[node] = ret[node] + 1;
+        jenkins_hash[node] = jenkins_hash[node] + 1;
+
+        calculate_hash_values_separate(machinecount, elementcount,key, &node, &index);
+        separate_hash[node] = separate_hash[node] + 1;
+
         i++;
     }
 
-    for(i=0; i<machinecount; i++) {
-        printf("Node: %03d, values: %05d  ", i, ret[i]);
-        for(j=0; j<ret[i] / 50; j++) {
-            printf("+");
-        }
-        printf("\n");
-    }
+    printf("Jenkins distribution:\n");
+    print_results(jenkins_hash, machinecount, elementcount);
+    printf("\n\n");
+    printf("Separate distribution:\n");
+    print_results(separate_hash, machinecount, elementcount);
+
 
 
     return 0;
+}
+int read_and_hash(int *jenkins_hash, int *separate_hash, int machinecount, int elementcount) {
 }
 
 
@@ -71,9 +80,37 @@ void calculate_hash_values(int machinecount, int elementcount, char *key, uint32
     uint8_t     node_part;
     uint32_t    local_part, key_hash;
     key_hash = jenkins_hash((uint8_t*)key, strlen(key));
-    //printf("key_hash: %d\n", key_hash);
-    separate(key_hash, &local_part, &node_part);
-    //*node = jenkins_hash(node_part, world_size);
     *node = key_hash % machinecount;
     *idx = key_hash % elementcount;
 }
+
+void calculate_hash_values_separate(int machinecount, int elementcount, char *key, uint32_t *node, uint32_t *idx) {
+    uint8_t     node_part;
+    uint32_t    local_part, key_hash;
+    key_hash = jenkins_hash((uint8_t*)key, strlen(key));
+    separate(key_hash, &local_part, &node_part);
+    *node = node_part % machinecount;
+    *idx = local_part % elementcount;
+}
+
+void print_results(int *array, int machinecount, int elementcount) {
+  int max = 1;
+  int i, j;
+  if((int) elementcount / machinecount > 100)
+    max = 15;
+  if((int)elementcount / machinecount > 1000)
+    max = 10;
+  if((int)elementcount / machinecount >= 10000)
+    max = 150;
+  if((int)elementcount / machinecount >= 100000)
+    max = 100;
+
+    for(i=0; i<machinecount; i++) {
+        printf("Node: %3d, values: %5d  ", i, array[i]);
+        for(j=0; j<array[i] / max; j++) {
+            printf("+");
+        }
+        printf("\n");
+    }
+}
+
